c3a6a7073e15cdd7b9eccdafadcade24cb47db5e
Praneepa Sithmi <sithmipraneepa1018@gmail.com>
2026-08-09 23:16:59 +0530

    feat: setup project folder architecture and core dependencies

---
+++ lib/core/constants/app_color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryMaroon = Color(0xFF670000);
+  static const Color darkCard = Color(0xFF4A0000);
+  static const Color lightBackground = Color(0xFFF8F5F5);
+  static const Color textDark = Color(0xFF222222);
+  static const Color cardBorder = Color(0xFFE0E0E0);
+}
